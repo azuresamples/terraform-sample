3373de31b313dccfa17a8e96252635ff3f6c8103
momoirozo2000 <9127468+momoirozo2000@users.noreply.github.com>
2018-11-01 11:34:26 +0900

    Create alibaba_test.tf

---
+++ alibaba_test.tf
@@ -0,0 +1,50 @@
+# Credential
+provider "alicloud" {
+    access_key = "xxxxxxxxxxxxxxxxx"
+    secret_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    region = "ap-northeast-1"
+}
+resource "alicloud_security_group" "group" {
+  name = "terraform-sg"
+  vpc_id = "${alicloud_vpc.vpc.id}"
+}
+resource "alicloud_security_group_rule" "allow_http" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "80/80"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.group.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+# VPCの作成
+resource "alicloud_vpc" "vpc" {
+  name = "terraform-vpc"
+  cidr_block = "10.1.0.0/21"
+}
+resource "alicloud_vswitch" "vsw" {
+  vpc_id            = "${alicloud_vpc.vpc.id}"
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.1.1.0/24"
+}
+# ECSに紐付けるEIP(グローバルIP)の作成
+resource "alicloud_eip" "eip" {
+  internet_charge_type = "PayByTraffic"
+}
+# EIPをECSと紐付け
+resource "alicloud_eip_association" "eip_asso" {
+  allocation_id = "${alicloud_eip.eip.id}"
+  instance_id   = "${alicloud_instance.web.id}"
+}
+
+# ECSの作成
+resource "alicloud_instance" "web" {
+  instance_name = "terraform-ecs"
+  availability_zone = "ap-northeast-1a"
+  image_id = "ubuntu_16_0402_64_20G_alibase_20180409.vhd" 
+  instance_type = "ecs.n4.small"
+  system_disk_category = "cloud_efficiency"
+  security_groups = ["${alicloud_security_group.group.id}"] 
+  vswitch_id = "${alicloud_vswitch.vsw.id}"
+}
