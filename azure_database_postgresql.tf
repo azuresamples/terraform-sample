b4a3d0dda3b9478fa5bf8b3d8382ce36526c68b6
azuresamples <54381444+azuresamples@users.noreply.github.com>
2019-12-05 18:21:08 +0900

    Update azure_database_postgresql.tf

--- azure_database_postgresql.tf
+++ azure_database_postgresql.tf
@@ -14,14 +14,13 @@ provider "azurerm" {
   tenant_id       = "${var.tenant_id}"
 }
 
-resource "azurerm_resource_group" "test" {
+data "azurerm_resource_group" "test" {
   name     = "suga-rsg"
-  location = "Japan East"
 }
 
 resource "azurerm_postgresql_server" "test" {
   name                = "suga-postgresql10-test"
-  location            = "${azurerm_resource_group.test.location}"
+  location            = "Japan East"
   resource_group_name = "${azurerm_resource_group.test.name}"
 
   sku {
@@ -46,7 +45,7 @@ resource "azurerm_postgresql_server" "test" {
 
 resource "azurerm_postgresql_database" "test" {
   name                = "exampledb"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  resource_group_name = "${data.azurerm_resource_group.test.name}"
   server_name         = "${azurerm_postgresql_server.test.name}"
   charset             = "UTF8"
   collation           = "C"
@@ -54,7 +53,7 @@ resource "azurerm_postgresql_database" "test" {
 
 resource "azurerm_postgresql_firewall_rule" "test" {
   name                = "AllowJumpbox"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  resource_group_name = "${data.azurerm_resource_group.test.name}"
   server_name         = "${azurerm_postgresql_server.test.name}"
   start_ip_address    = "${var.start_ip_address}"
   end_ip_address      = "${var.end_ip_address}"
@@ -62,7 +61,7 @@ resource "azurerm_postgresql_firewall_rule" "test" {
 
 #resource "azurerm_sql_virtual_network_rule" "test" {
 #    name                = "EndpointRule"
-#    resource_group_name = "${azurerm_resource_group.test.name}"
+#    resource_group_name = "${data.azurerm_resource_group.test.name}"
 #    server_name         = "${azurerm_postgresql_server.test.name}"
 #    subnet_id           = "/subscriptions/6d5f4926-9bf3-444c-97d9-a8625ebdc56e/resourceGroups/suga-rsg/providers/Microsoft.Network/virtualNetworks/suga-vnet/subnets/suga-subnet6"
 #}
